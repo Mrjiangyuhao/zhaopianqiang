374258e4c575eb6ad55046ee597801eadbe829a5
Mrjiangyuhao <690392431@qq.com>
2018-09-21 23:41:13 +0800

    修复chorme下，index溢出;采用自定义属性解决

--- js/load.js
+++ js/load.js
@@ -7,25 +7,26 @@ window.onload = function () {
     var Imax = null;
     var max1 = null;
     var ls = null;
-    var thisindex = null;
-    max1 = img[0].style.zIndex;
+    var thisindex;
     for (var i = 0; i < leg; i++) {
         //利用二维数组储存
         arr.push([img[i].offsetLeft, img[i].offsetTop]);
-        img[i].style.index = i;
+        //添加自定义属性记录下标。注意：慎用style.index，因为如果操作调换下标chorme会导致溢出 
+        img[i].index = i;
         (function () {
             var tem = i;      //采用闭包方式使新创建的变量留存在内存当中，重新申明多个tem ，不同的tem里面存储着不同的值       
             img[i].onmousedown = function (ev) {
-                
+
                 ev = ev || window.event;
                 var x = ev.clientX - img[tem].offsetLeft;
                 var y = ev.clientY - img[tem].offsetTop;
                 var This = this;
-                //可以不单独写函数获取最大值 ，采用Imax++累加 
-                Imax = Max(1)
-                if (Imax == "") {
-                    img[tem].style.zIndex += 1;
-
+                //可以不单独写函数获取最大值 ，采用Imax++来累加 
+                if (Imax == null) {
+                    Imax++;
+                }else{
+                    Imax++;
+                    img[tem].style.zIndex =  Imax;
                 }
                 if (Imax >= img[tem].style.zIndex) {
                     img[tem].style.zIndex = parseInt(Imax) + 1;
@@ -42,17 +43,17 @@ window.onload = function () {
                     var a = dista(This, img)
                     if (a) {
                         ls = a;
-                        a.style.border = "1px solid red";
+                        // a.style.border = "1px solid red";
                     }
 
                 }
                 Document.onmouseup = function () {
-                    for(var i = 0; i < leg ;i++){
+                    for (var i = 0; i < leg; i++) {
                         img[i].style.border = "";
                     }
                     //调换图片位置
-                    if( ls ){
-                        Hh(This,ls)
+                    if (ls) {
+                        Hh(This, ls)
                     }
                     Document.onmousemove = Document.onmouseup = null;
 
@@ -61,56 +62,37 @@ window.onload = function () {
 
         })()
     }
-    inpt.onclick = function aa(){
-    /*
-    Math.ceil();  //向上取整。 
-    Math.floor();  //向下取整。
-    Math.round();  //四舍五入。
-    */  
-       
-        var Lt = leg - 1;
-        var ram_1 = Math.round(Math.random()*Lt);
-        var ram_2 = Math.round(Math.random()*Lt);
-   
-        if( ram_1 != ram_2 ){
-            Hh(img[ram_1],img[ram_2])
-        }else{
-            //递归
-            aa();
-        }
-       
-         
+    inpt.onclick = function dg() {
+        /*
+        Math.ceil();  //向上取整。 
+        Math.floor();  //向下取整。
+        Math.round();  //四舍五入。
+        */
 
+        var Lt = leg - 1;
+        var ram_1 = Math.round(Math.random() * Lt);
+        var ram_2 = Math.round(Math.random() * Lt);
+        if (ram_1 != ram_2) {
+            Hh(img[ram_1], img[ram_2])
+        } else {
+            //递归
+            dg();
+        }
     }
-    
+
     posiT(img, arr)
 
-    // 递归找出最大值
-    function Max(i) {
-        var leng = img.length;
-        if (i == leng) {
-            return max1
-        }
-        if (max1 < parseInt(img[i].style.zIndex)) {
-            max1 = parseInt(img[i].style.zIndex);
-
-        }
-        return Max(i + 1)
-    }
-
     //换位
-    function Hh(obj1,obj2){
-        // console.log(obj1,obj2)
-        obj1.style.left = arr[obj2.style.index][0] + "px";
-        obj1.style.top = arr[obj2.style.index][1] + "px";    
-        obj2.style.left = arr[obj1.style.index][0] + "px";
-        obj2.style.top = arr[obj1.style.index][1] + "px";
-       //换位后将下标也一起调换，不然调换完后无法调换回来
-        thisindex = obj1.style.index;
-        obj1.style.index = obj2.style.index;
-        obj2.style.index = thisindex;
+    function Hh(obj1, obj2) {
+        obj1.style.left = arr[obj2.index][0] + "px";
+        obj1.style.top = arr[obj2.index][1] + "px";
+        obj2.style.left = arr[obj1.index][0] + "px";
+        obj2.style.top = arr[obj1.index][1] + "px";
+        //    换位后将下标也一起调换，不然调换完后无法调换回来
+        thisindex = obj1.index;
+        obj1.index = obj2.index;
+        obj2.index = thisindex;
     }
-
 }
 //动态实现定位
 function posiT(obj, ar) {
@@ -167,15 +149,15 @@ function gg(obj1, obj2) {
     //sqrt (a²+b²)=c²开平方根
     return Math.sqrt(objLeft * objLeft + objtop * objtop)
 }
-//找出最大值
-// function Max(obj) {
-//     var leng = obj.length;
-//     var t = 0;
-//     for (var i = 0; i < leng; i++) {
-//         if (t < obj[i].style.zIndex) {
-//             t = obj[i].style.zIndex;   // 注意返回的是字符串，因为zIndex是字符串类型赋值过去所以导致String
-//         }
-//     }
+    // 递归找出最大值
+    // function Max(i) {
+    //     var leng = img.length;
+    //     if (i == leng) {
+    //         return max1
+    //     }
+    //     if (max1 < parseInt(img[i].style.zIndex)) {
+    //         max1 = parseInt(img[i].style.zIndex);
 
-//     return t
-// }
+    //     }
+    //     return Max(i + 1)
+    // }
